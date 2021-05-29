283f1d7cce586329074981d1b50fa67ddf1885f8
NazmiKameri <76222684+NazmiKameri@users.noreply.github.com>
2021-05-29 16:08:02 +0200

    Krijimi i frontend KompaniaProdhuese

---
+++ my-app/src/AddKompModal.js
@@ -0,0 +1,77 @@
+import React,{Component} from 'react';
+import {Modal,Button, Row, Col, Form} from 'react-bootstrap';
+
+export class AddKompModal extends Component{
+    constructor(props){
+        super(props);
+        this.handleSubmit=this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        fetch(process.env.REACT_APP_API+'kompaniaprodhuese',{
+            method:'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+            
+                EmriQytetit:event.target.EmriQytetit.value
+            })
+        })
+        .then(res=>res.json())
+        .then((result)=>{
+            alert(result);
+        },
+        (error)=>{
+            alert('Failed');
+        })
+    }
+    render(){
+        return (
+            <div className="container">
+
+<Modal
+{...this.props}
+size="lg"
+aria-labelledby="contained-modal-title-vcenter"
+centered
+>
+    <Modal.Header clooseButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+            Add Kompani
+        </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+
+        <Row>
+            <Col sm={6}>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group controlId="EmriKompanis">
+                        <Form.Label>EmriKompanis</Form.Label>
+                        <Form.Control type="text" name="EmriKompanis" required 
+                        placeholder="EmriKompanis"/>
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Button variant="primary" type="submit">
+                            Add EmriKompani
+                        </Button>
+                    </Form.Group>
+                </Form>
+            </Col>
+        </Row>
+    </Modal.Body>
+
+    <Modal.Footer>
+        <Button variant="danger" onClick={this.props.onHide}>Close</Button>
+    </Modal.Footer>
+
+</Modal>
+
+            </div>
+        )
+    }
+
+} 
