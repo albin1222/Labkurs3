6cafbcd4cfd764074e44843352d45b78df7584b8
NazmiKameri <76222684+NazmiKameri@users.noreply.github.com>
2021-06-05 23:13:11 +0200

    Krijimi i Databazes,Models per pjesen e PC Builder dhe Controlleri i Pjeses Procesori

--- Labkurs3/PjesaESQL.sql
+++ Labkurs3/PjesaESQL.sql
@@ -70,3 +70,105 @@ insert into dbo.PreferencatETel values ('Iphone x','Rose gold max', '2','64','no
   select * from dbo.Phone
   insert into dbo.Phone values ('Iphone','Apple','','none')
 
+
+  --Krijimi  i pjesve te pc builder
+  create table dbo.Procesori(
+Id int identity(1,1),
+LlojiFirmes varchar(500),
+Gjenerata varchar(500),
+Specifika varchar(500),
+Qmimi float
+) 
+insert into dbo.Procesori values ('Intel','Gen9', 'I9',133.22)
+select *from dbo.Procesori
+
+create table dbo.Cooling(
+Id int identity(1,1),
+LlojiFirmes varchar(500),
+Verzioni varchar(500),
+Qmimi float
+) 
+insert into dbo.Cooling values ('Jonsbo','CR-110ARGB FAN00068',46)
+select *from dbo.Cooling
+
+create table dbo.Motherboard(
+Id int identity(1,1),
+LlojiFirmes varchar(500),
+Verzioni varchar(500),
+Qmimi float
+) 
+insert into dbo.Motherboard values ('Asus','rog strix b460-g gaming micro atx',200)
+select *from dbo.Motherboard
+
+create table dbo.RAM(
+Id int identity(1,1),
+LlojiFirmes varchar(500),
+Verzioni varchar(500),
+GB varchar(500),
+Qmimi float
+) 
+insert into dbo.RAM values ('Apacer','2666Mhz','8GB',52.5)
+select *from dbo.RAM
+
+create table dbo.RAM2(
+Id int identity(1,1),
+LlojiFirmes varchar(500),
+Verzioni varchar(500),
+GB varchar(500),
+Qmimi float
+) 
+insert into dbo.RAM2 values ('Apacer','2666Mhz','8GB',52.5)
+select *from dbo.RAM2
+
+create table dbo.SolidStateDrive(
+Id int identity(1,1),
+LlojiFirmes varchar(500),
+Verzioni varchar(500),
+GB varchar(500),
+Qmimi float
+) 
+insert into dbo.SolidStateDrive values ('Kioxia','SSD00017','240GB',40.5)
+select *from dbo.SolidStateDrive
+
+create table dbo.HardDiskDrive(
+Id int identity(1,1),
+LlojiFirmes varchar(500),
+Verzioni varchar(500),
+GB varchar(500),
+Qmimi float
+) 
+insert into dbo.HardDiskDrive values ('Toshiba','P300','1TB',40.5)
+select *from dbo.HardDiskDrive
+
+create table dbo.GraphicsCard(
+Id int identity(1,1),
+LlojiFirmes varchar(500),
+Verzioni varchar(500),
+GB varchar(500),
+Qmimi float
+) 
+insert into dbo.GraphicsCard values ('Nividia','GTX 1050 Ti','4GB',300)
+select *from dbo.GraphicsCard
+
+create table dbo.PowerSupply(
+Id int identity(1,1),
+LlojiFirmes varchar(500),
+Verzioni varchar(500),
+KW varchar(500),
+Qmimi float
+) 
+insert into dbo.PowerSupply values ('RPC','PSU000024','450W',15)
+select *from dbo.PowerSupply
+
+create table dbo.CaseN(
+Id int identity(1,1),
+LlojiFirmes varchar(500),
+Verzioni varchar(500),
+Tipi varchar(500),
+Qmimi float
+) 
+insert into dbo.CaseN values ('NZCT','CAS000033','MiniTower',105)
+select *from dbo.CaseN
+
+
+
