ffc8d290256b601842e5dc217b85fbbac7a6b50d
LeonMuqaj <84706537+LeonMuqaj@users.noreply.github.com>
2021-06-07 20:06:08 +0200

    Pjeset Per PC added

---
+++ Labkurs3/Controllers/PjesetPerPCController.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using Labkurs3.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Labkurs3.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PjesetPerPCController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+
+        public PjesetPerPCController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                    select IDPjesa, Emri, Sasia, Specifikat, Cmimi from dbo.PjesetPerPC";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("Labkurs3AppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(PjesetPerPC pjs)
+        {
+            string query = @"
+                    insert into dbo.PjesetPerPC values
+                    ('" + pjs.Emri + @"'
+                    ,'" + pjs.Sasia + @"'
+                    ,'" + pjs.Specifikat + @"'
+                    ,'" + pjs.Cmimi + @"'
+                    )
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("Labkurs3AppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Succesfully");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(PjesetPerPC pjs)
+        {
+            string query = @"
+                    update dbo.PjesetPerPC set 
+                    Emri = '" + pjs.Emri + @"'
+                    ,Sasia = '" + pjs.Sasia + @"'
+                    ,Specifikat = '" + pjs.Specifikat + @"'
+                    ,Cmimi = '" + pjs.Cmimi + @"'
+                    where IDPjesa = " + pjs.IDPjesa + @"
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("Labkurs3AppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated Succesfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    delete from dbo.PjesetPerPC 
+                    where IDPjesa = " + id + @"
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("Labkurs3AppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted Succesfully");
+        }
+    }
+}
+
