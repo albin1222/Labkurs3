14a4164ab601baf85c0b9bb714f82e20b2d739a4
LeonMuqaj <84706537+LeonMuqaj@users.noreply.github.com>
2021-06-07 20:04:36 +0200

    Add files via upload

---
+++ Labkurs3/Models/PjesetPerPC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labkurs3.Models
+{
+    public class PjesetPerPC
+    {
+        public int IDPjesa { get; set; }
+        public string Emri { get; set; }
+        public int Sasia { get; set; }
+        public string Specifikat { get; set; }
+        public string Cmimi { get; set; }
+    }
+}
